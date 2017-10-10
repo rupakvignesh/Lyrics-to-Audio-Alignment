ce2e6fbc717b2cbd7519b6f05aa9c66095c696d6
bgenchel <benjiegenchel@gmail.com>
2017-10-10 18:54:41 -0400

    don't leave sections smaller than threshold length, instead, append them to the next section. Ignore msaf generated file and also testing directory

--- Damp_Dataset/scripts/msaf_segmentation.py
+++ Damp_Dataset/scripts/msaf_segmentation.py
@@ -5,18 +5,33 @@ import scipy.io.wavfile as wavfile
 import os, sys
 import os.path as op
 
+MIN_SEG_LEN = 1  # seconds
+
 def main(wavlist, outdir):
     for f in wavlist:
         if f.endswith('.wav'):
             print("processing {}".format(f))
             boundaries, _ = msaf.process(f)
             rate, data = wavfile.read(f)
+            buff = None
+            segcnt = 1
+            import pdb
+            pdb.set_trace()
             for b_ind in xrange(1, len(boundaries)):
                 segment = data[int(np.round(rate*(boundaries[b_ind - 1]))):int(round(rate*(boundaries[b_ind])))]
-                outfilename = op.join(outdir,
-                                      "{}-{}.wav".format(op.splitext(op.basename(f))[0], b_ind))
-                wavfile.write(outfilename, rate, segment)
-                print("{} created.".format(outfilename))
+                if buff is None:
+                    buff = segment
+                else:
+                    buff = np.concatenate((buff, segment), axis=0)
+                if len(buff) > rate*MIN_SEG_LEN:
+                    outfilename = op.join(outdir,
+                                          "{}-{}.wav".format(
+                                              op.splitext(op.basename(f))[0],
+                                              segcnt))
+                    wavfile.write(outfilename, rate, buff)
+                    print("{} created.".format(outfilename))
+                    segcnt += 1
+                    buff = None
 
 if __name__ == '__main__':
     wavdir = sys.argv[1]
