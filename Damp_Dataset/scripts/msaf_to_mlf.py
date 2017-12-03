769cac5443bbf137fa1a5818c280bff6c86c04a3
Rupak Vignesh Swaminathan <rupakvignesh@gmail.com>
2017-12-02 20:41:53 -0500

    completed boundaries_id

--- Damp_Dataset/scripts/msaf_to_mlf.py
+++ Damp_Dataset/scripts/msaf_to_mlf.py
@@ -9,9 +9,10 @@ import numpy as np
 import scipy.io.wavfile as wavfile
 import os, sys
 import os.path as op
+import pdb
 
-lab_file_path = ''
-msaf_segments_path = ''
+lab_file_path = '/Users/RupakVignesh/Desktop/fall16/7100/Lyrics-to-Audio-Alignment/Damp_Dataset/test_lab_word_damp_model/'
+msaf_segments_path = '/Users/RupakVignesh/Desktop/fall16/7100/Lyrics-to-Audio-Alignment/Damp_Dataset/Audio_test_segmentation/'
 
 def make_mlf(wav_segment_name,lyric_lines):
     """
@@ -36,22 +37,43 @@ def get_boundaries_id(wav_id):
     get all segments starting with wav_id and
     output a list of time stamps corresponding to msaf segments.
     """
+    segment_list = [wav_seg for wav_seg in os.listdir(msaf_segments_path) if (wav_seg.startswith(wav_id) and wav_seg.endswith('wav'))]
+    boundaries_id = []
+    for i in segment_list:
+        [fs,x] = wavfile.read(msaf_segments_path+i)
+        dur = float(len(x))/fs
+        boundaries_id.append(dur)
 
-
-
+    boundaries_id = np.cumsum(boundaries_id)
+    return boundaries_id
 
 
 
 def main():
     #Read labfile list
+    with open(sys.argv[1],'r') as F:
+        labfile_list = [lines.strip() for lines in F]
+    F.close()
 
-    #Get msaf boundaries for each file in list
+    for i in range(len(labfile_list)):
+        with open(lab_file_path + labfile_list[i], 'r') as F:
+            lablines = [ str.split(lines.strip()) for lines in F]
+        F.close()
+        for j in range(len(lablines)):
+            #Convert from 100ns to seconds
+            lablines[j][0] = float(lablines[j][0])/(10**7)
+            lablines[j][1] = float(lablines[j][1])/(10**7)
 
+        #Get msaf boundaries for each file in list
+        wav_id = op.splitext(labfile_list[i])
+        boundaries_id = get_boundaries_id(wav_id)
+        pdb.set_trace()
     #Approximate to lab file time stamp
+
 
     #Get lyric segments within msaf boundaries
 
-    #Make mlf 
+    #Make mlf
 
 
 
